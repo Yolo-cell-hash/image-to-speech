544476f1f78cbdaa09bec4e6fc652b251ca8f575
Yolo-cell-hash <keerjar@gmail.com>
2023-06-13 18:30:34 +0530

    Update to empty local repo after upload

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const bucketName = 'image_to_speech_file_storage';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 const timestamp = Date.now();
 const upload = multer({ dest: 'uploads/' });
@@ -29,7 +29,7 @@ app.post('/', upload.single('imageFile'), (req, res) => {
   const imageFile = req.file;
   const fileName = 'uploaded_image.png';
   const filePath = path.join(__dirname, 'uploads', fileName);
-  let localFilePath= filePath;
+  let localFilePath = filePath;
   const fileExtension = path.extname(localFilePath);
   let destinationFileName = `image_${timestamp}${fileExtension}`;
 
@@ -46,27 +46,34 @@ app.post('/', upload.single('imageFile'), (req, res) => {
         const storage = new Storage();
         const bucket = storage.bucket(bucketName);
         const file = bucket.file(destinationFileName);
-    
+
         const readStream = fs.createReadStream(localFilePath);
-    
+
         const uploadStream = file.createWriteStream({
           resumable: false,
           contentType: 'image/jpeg',
           predefinedAcl: 'publicRead', // Set the desired ACL for the uploaded file
         });
-    
+
         readStream.pipe(uploadStream);
-    
+
         return new Promise((resolve, reject) => {
           uploadStream.on('error', (err) => {
             console.error('Error uploading image:', err);
             reject(err);
           });
-    
+
           uploadStream.on('finish', () => {
             console.log('Image uploaded successfully!');
             resolve(true);
-            res.redirect('/')
+            fs.unlink(localFilePath, (err) => {
+              if (err) {
+                console.error('Error deleting local image file:', err);
+              } else {
+                console.log('Local image file deleted successfully');
+              }
+            });
+            res.redirect('/');
           });
         });
       } catch (err) {
@@ -76,13 +83,11 @@ app.post('/', upload.single('imageFile'), (req, res) => {
       }
     }
 
-uploadImageToBucket(bucketName, localFilePath, destinationFileName);
-
-
+    uploadImageToBucket(bucketName, localFilePath, destinationFileName);
   });
 });
 
 let port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log("Server activated at port successfully");
+  console.log('Server activated at port successfully');
 });
