c7a3158f6f98c280aa27934bcf8373868b0ae72a
Yolo-cell-hash <keerjar@gmail.com>
2023-06-15 18:29:13 +0530

    Storage replaced with S3 instead of GCP bucket

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
-const { Storage } = require('@google-cloud/storage');
-const bucketName = 'image_to_speech_file_storage';
+
+const bucketName = process.env.bucketName;
+const accessKeyId = process.env.accessKeyId;
+const secretAccessKey = process.env.secretAccessKey;
 
 const app = express();
-const storage = new Storage();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -20,21 +23,6 @@ const upload = multer({ dest: 'uploads/' });
 app.get('/', function (req, res) {
   res.render('login');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.get('/app', function (req, res) {
   res.render('home');
@@ -48,9 +36,6 @@ app.post('/app', upload.single('imageFile'), (req, res) => {
   const imageFile = req.file;
   const fileName = 'uploaded_image.png';
   const filePath = path.join(__dirname, 'uploads', fileName);
-  let localFilePath = filePath;
-  const fileExtension = path.extname(localFilePath);
-  let destinationFileName = `image_${timestamp}${fileExtension}`;
 
   fs.rename(imageFile.path, filePath, async (err) => {
     if (err) {
@@ -60,49 +45,37 @@ app.post('/app', upload.single('imageFile'), (req, res) => {
 
     console.log('Image saved locally successfully');
 
-    async function uploadImageToBucket(bucketName, localFilePath, destinationFileName) {
-      try {
-        const storage = new Storage();
-        const bucket = storage.bucket(bucketName);
-        const file = bucket.file(destinationFileName);
+    AWS.config.update({
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey,
+    });
 
-        const readStream = fs.createReadStream(localFilePath);
+    const s3 = new AWS.S3();
 
-        const uploadStream = file.createWriteStream({
-          resumable: false,
-          contentType: 'image/jpeg',
-          predefinedAcl: 'publicRead', // Set the desired ACL for the uploaded file
-        });
+    const params = {
+      Bucket: bucketName,
+      Key: `image_${timestamp}.png`,
+      Body: fs.createReadStream(filePath),
+      ACL: 'public-read',
+      ContentType: 'image/png',
+    };
 
-        readStream.pipe(uploadStream);
-
-        return new Promise((resolve, reject) => {
-          uploadStream.on('error', (err) => {
-            console.error('Error uploading image:', err);
-            reject(err);
-          });
-
-          uploadStream.on('finish', () => {
-            console.log('Image uploaded successfully!');
-            resolve(true);
-            fs.unlink(localFilePath, (err) => {
-              if (err) {
-                console.error('Error deleting local image file:', err);
-              } else {
-                console.log('Local image file deleted successfully');
-              }
-            });
-            res.redirect('/app');
-          });
-        });
-      } catch (err) {
+    s3.upload(params, (err, data) => {
+      if (err) {
         console.error('Error uploading image:', err);
-        return false;
-        res.redirect('/app');
+        return res.status(500).json({ error: 'Failed to upload image' });
       }
-    }
 
-    uploadImageToBucket(bucketName, localFilePath, destinationFileName);
+      console.log('Image uploaded successfully!');
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error('Error deleting local image file:', err);
+        } else {
+          console.log('Local image file deleted successfully');
+        }
+      });
+      res.redirect('/app');
+    });
   });
 });
 
