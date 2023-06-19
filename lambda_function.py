41f54a8742fb8e5b23b049223a485695fc4d18ba
Yolo-cell-hash <keerjar@gmail.com>
2023-06-20 02:10:25 +0530

    Patch to adjust Polly playback issue

--- lambda_function.py
+++ lambda_function.py
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
         blocks = response['Blocks']
         for block in blocks:
             if block['BlockType'] == 'LINE':
-                extracted_text += block['Text'] + '\n'
+                extracted_text += block['Text'] + '\n\n'
 
         destination_key_name = source_key_name.rsplit('.', 1)[0] + '.txt'
         s3.put_object(Body=extracted_text, Bucket=destination_bucket_name, Key=destination_key_name)
